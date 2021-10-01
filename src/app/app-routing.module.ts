eaac79b408be838bd6c30316c04a5d47ec32cb5c
Guilherme F. Ferreira <gferreira@itelinfo.com>
2021-10-01 17:42:33 -0400

    Clube alterar, incluir, listar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { AlterarClubeComponent } from './alterar-clube/alterar-clube.component';
 import { IncluirClubeComponent } from './incluir-clube/incluir-clube.component';
+import { ListarJogoComponent } from './jogo-listar/listar-jogo.component';
 import { ListarClubeComponent } from './listar-clube/listar-clube.component';
 
 const routes: Routes = [
@@ -10,12 +10,8 @@ const routes: Routes = [
   {path:'incluirclube', component: IncluirClubeComponent},
   {path:'alterarclube', component: AlterarClubeComponent},
   {path:'alterarclube/:id', component: AlterarClubeComponent},
-=======
-import { ListarJogoComponent } from './jogo-listar/listar-jogo.component';
-
-const routes: Routes = [
   {path:'listarjogos', component: ListarJogoComponent}
->>>>>>> 5ebc97a1a6f55b5b4c1ba3283c4ac99c1c5a186f
+
 ];
 
 @NgModule({
