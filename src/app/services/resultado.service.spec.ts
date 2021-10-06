4056049aa82398167fc17a33a538e411df19b451
Kevin Olentino <kevinolentino@gmail.com>
2021-10-06 04:27:43 -0400

    Update resultado.service.spec.ts

--- src/app/services/resultado.service.spec.ts
+++ src/app/services/resultado.service.spec.ts
@@ -1,16 +1,50 @@
+// Http testing module and mocking controller
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+// Other imports
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClient, HttpErrorResponse, HttpHandler } from '@angular/common/http';
 import { ResultadoService } from './resultado.service';
+import * as $ from 'jquery'
 
-describe('ResultadoService', () => {
+interface Resultado{
+  idResultado: number;
+  nome:string;
+  vencedor:boolean;
+  idJogo:number;
+  idClube:number;
+}
+
+const testUrl = 'http://localhost:8080/resultado/listar';
+
+describe('Teste com ajax', () => {
+  let httpClient: HttpClient;
+  let handler: HttpHandler;
+  let httpTestingController: HttpTestingController;
   let service: ResultadoService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ResultadoService);
-  });
+    // Inject the http service and test controller for each test      
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule],
+      providers:[ResultadoService]
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+    })
   });
-});
+  it('Teste de Listar do componente com BackEnd', function(doneFn) {
+    $.ajax({
+        url: 'http://localhost:8080/resultado/listar',
+        dataType: 'json',
+        success: function (data: Resultado[], response: any) {
+            // Here your expected using data
+            expect(data.length).toBeGreaterThanOrEqual(0)
+            doneFn();
+        },
+        error: function( data, response){
+          expect(() => { "Erro ao Testar" }).toThrow("Erro ao testar")
+        }
+    });
+  }); 
+    
+
+})
