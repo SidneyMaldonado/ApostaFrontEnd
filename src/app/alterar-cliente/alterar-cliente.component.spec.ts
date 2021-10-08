fb945dc5a75a9c3422d5ec7f3c93b4e43d3ff218
Mateus Tomi <mtomi@itelinfo.com>
2021-10-08 15:15:16 -0400

    AlterarClienteTest

--- src/app/alterar-cliente/alterar-cliente.component.spec.ts
+++ src/app/alterar-cliente/alterar-cliente.component.spec.ts
@@ -9,6 +9,9 @@ import { ActivatedRoute, RouterModule } from '@angular/router';
 describe('AlterarClienteComponent', () => {
   let component: AlterarClienteComponent;
   let fixture: ComponentFixture<AlterarClienteComponent>;
+  let h1: HTMLElement;
+
+  let app: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -22,9 +25,24 @@ describe('AlterarClienteComponent', () => {
     .compileComponents();
     fixture = TestBed.createComponent(AlterarClienteComponent);
     component = fixture.debugElement.componentInstance;
+    app = fixture.debugElement.nativeElement;
  
   });
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('#Title - O titulo deve constar no html', () => {
+    let expected: string  = 'Alterar Cliente';
+    h1 = app.querySelector('h1');
+    let result = h1.innerHTML;
+    expect(result).toContain(expected);
+
+  });
+
+  it('#Botao enviar - O botão enviar deve aparecer no html', () =>{
+    let expected: string = 'Enviar';
+    h1 = app.querySelector('.btn');
+    let result= h1.innerHTML;
+    expect(result).toEqual(expected);
   });
 });
