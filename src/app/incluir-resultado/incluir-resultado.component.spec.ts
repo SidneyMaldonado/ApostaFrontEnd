9e594b62eebb7ce51ac2bcf4330315fc62d1e548
Douglas Y. Yabusame <dyabusame@itelinfo.com>
2021-10-08 16:10:56 -0400

    Incluir-resultado

--- src/app/incluir-resultado/incluir-resultado.component.spec.ts
+++ src/app/incluir-resultado/incluir-resultado.component.spec.ts
@@ -10,6 +10,9 @@ import { IncluirResultadoComponent } from './incluir-resultado.component';
 describe('IncluirResultadoComponent', () => {
   let component: IncluirResultadoComponent;
   let fixture: ComponentFixture<IncluirResultadoComponent>;
+  let h1: HTMLElement;
+  let field: HTMLInputElement;
+  let app: any; 
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,13 +20,32 @@ describe('IncluirResultadoComponent', () => {
       providers: [JogoService, ClubeService, ResultadoService],
       declarations: [ IncluirResultadoComponent ]
     })
-    .compileComponents();
+    .compileComponents();   
+  });
+  beforeEach(() => {
     fixture = TestBed.createComponent(IncluirResultadoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    app=fixture.debugElement.nativeElement;
   });
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  
+  it('#Title - O titulo deve aparecer no HTML', () => {
+    let expected:string = 'Incluir Resultado';
+    h1 = app.querySelector('h1');
+    let result = h1.innerHTML;
+    expect(result).toContain(expected);
+  });
+
+  it('#Botão enviar - O botão enviar deve aparecer no html', () => {
+  let expected: string = 'Enviar';
+  h1= app.querySelector('.incluirresultado');
+  let result= h1.innerHTML;
+  expect(result).toEqual(expected);
+
   });
 });
