33bd0704ab5e3c2074e31379cfe4ac3edffd4e0f
Kevin Olentino <32975170+KevinOlentino@users.noreply.github.com>
2021-10-08 15:58:34 -0400

    Adicionar listar-estadio

--- src/app/listar-estadio/listar-estadio.component.spec.ts
+++ src/app/listar-estadio/listar-estadio.component.spec.ts
@@ -7,6 +7,9 @@ import { ListarEstadioComponent } from './listar-estadio.component';
 describe('ListarEstadioComponent', () => {
   let component: ListarEstadioComponent;
   let fixture: ComponentFixture<ListarEstadioComponent>;
+  let h1: HTMLElement;
+  let field: HTMLInputElement;
+  let app: any; 
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,15 +18,29 @@ describe('ListarEstadioComponent', () => {
       declarations: [ ListarEstadioComponent ]
     })
     .compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(ListarEstadioComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    app=fixture.debugElement.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#Title - O titulo deve aparecer no HTML', () => {
+    let expected:string = 'Listar Estadios';
+    h1 = app.querySelector('h1');
+    let result = h1.innerHTML;
+    expect(result).toContain(expected);
+  });
+
+  it('#Botão enviar - O botão enviar deve aparecer no html', () => {
+  let expected: string = 'Incluir Estadio';
+  h1= app.querySelector('.incluirestadio');
+  let result= h1.innerHTML;
+  expect(result).toEqual(expected);
+
   });
 });
